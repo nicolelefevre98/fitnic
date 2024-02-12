e7f70a30b1a8897cc7c5cd1d4ba5f0a400a176a8
Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>
2024-02-12 20:54:36 +0100

    Added first view
    View is like a div

--- Fitnic/App.js
+++ Fitnic/App.js
@@ -1,10 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import exercises from './assets/data/exercises.json';
 
 export default function App() {
+  const exercise = exercises[0];
+  console.log(exercises[0]);
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <View style={styles.exerciseContainer}>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <Text style={styles.exerciseSubtitle}>{capitalizeFirstLetter(exercise.muscle)} | {capitalizeFirstLetter(exercise.equipment)} </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gainsboro',
     justifyContent: 'center',
+    padding: 10,
+  },
+  exerciseContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+  },
+  exerciseName: {
+    fontSize: 20, 
+    fontWeight: '600',
+  },
+  exerciseSubtitle: {
+    color: 'dimgray'
   },
 });
