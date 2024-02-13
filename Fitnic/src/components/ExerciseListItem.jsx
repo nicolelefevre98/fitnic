9e686af4102b355342d5a80806cf45d1cd3096ca
Nicole le Fevre <83703145+nicolelefevre98@users.noreply.github.com>
2024-02-13 16:05:23 +0100

    Setup Expo Router

--- Fitnic/src/components/ExerciseListItem.jsx
+++ Fitnic/src/components/ExerciseListItem.jsx
@@ -18,6 +18,16 @@ export default function ExerciseListItem({ item }) {
       padding: 10,
       borderRadius: 10,
       gap: 5,
+      marginHorizontal: 2,
+
+      // Shadow
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.2,
+      shadowRadius: 1.41,
     },
     exerciseName: {
       fontSize: 20, 
